b4c03fb7041a6d0372c0c089bccc340723e62435
amisukanta <amisukanta02@gmail.com>
2015-09-16 01:46:33 +0200

    Add devise test helpers with rspec and create email sequence in user factories to test user log in

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    email "suka@test.com"
+    sequence(:email) { |n| "person_#{n}@example.com" }
     password "12345678"
+    password_confirmation "12345678"
   end
 
 end
--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -2,7 +2,8 @@ require "rails_helper"
 
 feature "user log in" do
   scenario "allows an existing user to sing in" do
-    user = create(:user)
+    password = "123456789"
+    user = build( :user, password: password, password_confirmation: password )
     visit "users/sign_in"
 
     fill_in "Email", with: user.email
