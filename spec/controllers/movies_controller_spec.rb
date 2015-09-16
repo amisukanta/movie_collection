5e052a51105398ac72ea60ef668d873967eafac4
amisukanta <amisukanta02@gmail.com>
2015-09-16 23:09:44 +0200

    Annonymous user will be redirected to signin

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -7,7 +7,15 @@ describe MoviesController do
       get :index
       expect(response).to be_success
       expect(response).to render_template('index')
-      
+    end
+    describe "anonymous user" do
+      before :each do
+        login_with nil
+      end
+      it "should redirect users to signin" do
+        get :index
+        expect(response).to redirect_to( new_user_session_path )
+      end
     end
   end
 end
